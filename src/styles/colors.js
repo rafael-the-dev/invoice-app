667409aa97268a11b43a153df07658e2d3bb7ef7
Rafael Tivane <luislangabusiness@gmail.com>
2021-12-06 22:02:18 +0200

    adding colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,3 +1,14 @@
 export const colors = {
-    
+    dark1: '#141625',
+    dark2: '#0C0E16',
+    darkBlue: '#1E2139',
+    darkPink: '#EC5757',
+    darkGrey: '#7E88C3',
+    darkPurple: '#7C5DFA',
+    grey: '#888EB0',
+    lightBg: '#F8F8FB (Light BG)',
+    lightBlue: '#252945',
+    lightGrey: '#DFE3FA',
+    lightPurple: '#9277FF',
+    lightPink: '#9277FF'
 }
