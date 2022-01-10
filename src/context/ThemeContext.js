e9ea819290bed02f86cd0415374758f66c9fc892
Rafael Tivane <luislangabusiness@gmail.com>
2022-01-10 07:41:40 +0200

    keeping track of app theme and setting back theme based on last theme after refreshing

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -8,6 +8,21 @@ export const ThemeContextProvider = ({ children }) => {
 
     const toggleTheme = useCallback(() => setTheme(theme => ({ ...theme, isLightTheme: !theme.isLightTheme})), []);
 
+
+    useEffect(() => {
+        if(!localStorage.getItem('invoice-app__isLightTheme')) {
+            localStorage.setItem('invoice-app__isLightTheme', JSON.stringify(false));
+        }
+    }, []);
+
+    useEffect(() => {
+        const stringifiedTheme = localStorage.getItem('invoice-app__isLightTheme');
+        if(stringifiedTheme) {
+            const storedTheme = JSON.parse(stringifiedTheme);
+            setTheme({ isLightTheme: storedTheme});
+        }
+    }, []);
+
     useEffect(() => {
         if(theme.isLightTheme) {
             document.querySelector('html').classList.add('light-theme');
@@ -16,6 +31,7 @@ export const ThemeContextProvider = ({ children }) => {
             document.querySelector('html').classList.remove('light-theme');
             document.querySelector('html').classList.add('dark-theme');
         }
+        localStorage.setItem('invoice-app__isLightTheme', JSON.stringify(theme.isLightTheme));
     }, [ theme ])
 
     return (
